665b28733685c2b4b06ac074d68e4a0d3d16ae62
Cassie Capewell <cassie.capewell@gmail.com>
2021-07-21 10:29:47 -0700

    july 21 codewars

---
+++ codewars-july-21/main.js
@@ -0,0 +1,18 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function that takes a list of strings and returns each line prepended by the correct number.
+//
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+//
+// Examples:
+//
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+//
+
+let number = function(array) {
+  return array.map(function (line, index) {
+    return (index + 1) + ": " + line;
+  });
+}
+console.log(number(['a', 'b', 'c']))
